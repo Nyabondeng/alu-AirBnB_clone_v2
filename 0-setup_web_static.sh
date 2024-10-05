23b6b327d4f38d462e9f3ead471231a16eaef2fd
Nyabondeng <n.adut1@alustudent.com>
2024-10-05 21:00:34 +0300

    Web setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,14 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Write a Bash script that sets up your web servers for the deployment of web_static
 
 apt-get update
-apt-get install -y nginx
+apt-get -y install nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+
+echo "<h1>Holberton School</h1>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
